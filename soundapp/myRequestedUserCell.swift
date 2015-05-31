190d7fbe10bf3a089406bb0f57d670e2f59b7478
Alicia Iott <aliciamiott@gmail.com>
2015-05-30 23:50:46 -0400

    just in case

--- soundapp/myRequestedUserCell.swift
+++ soundapp/myRequestedUserCell.swift
@@ -7,9 +7,60 @@
 //
 
 import UIKit
+import Parse
 
 class myRequestedUserCell: UITableViewCell {
 
-
+    let currentUser = PFUser.currentUser()
+    var friend = PFUser()
+    
     @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBAction func acceptRequest(sender: AnyObject) {
+        
+        println("accepting request you stupid butthead")
+        var query = PFUser.query()
+        query!.whereKey("username", equalTo: userNameLabel.text!)
+        query!.getFirstObjectInBackgroundWithBlock{
+            (user: AnyObject?, error: NSError?) -> Void in
+            if !(error != nil) {
+                println("hereeee")
+                self.friend = user as! PFUser!
+                self.queryPending()
+            }
+            else{
+                println(error)
+            }
+        }
+        
+    }
+    
+    func queryPending(){
+        
+        var pendingQuery = PFQuery(className:"PendingTable")
+        pendingQuery.whereKey("to", equalTo: PFUser.currentUser()!)
+        pendingQuery.whereKey("from", equalTo: self.friend)
+        pendingQuery.findObjectsInBackgroundWithBlock {
+            (requests: [AnyObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                for request in requests! {
+                    request.deleteInBackground()
+                    self.addToEachFriendsTable()
+                    NSNotificationCenter.defaultCenter().postNotificationName("load", object: nil)
+                }
+            } else {
+                println("Error: \(error!) \(error!.userInfo!)")
+            }
+        }
+        
+    }
+    
+    func addToEachFriendsTable(){
+        
+        println("adding to friends table here")
+        
+        
+    }
+
 }
--- soundapp/requestedUserCell.swift
+++ soundapp/requestedUserCell.swift
@@ -10,5 +10,47 @@ import UIKit
 
 class requestedUserCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
-
+/*
+    
+    @IBAction func acceptRequest(sender: AnyObject) {
+    
+    println("accepting request you stupid butthead")
+    var query = PFUser.query()
+    query!.whereKey("username", equalTo: userNameLabel.text!)
+    query!.getFirstObjectInBackgroundWithBlock{
+    (user: AnyObject?, error: NSError?) -> Void in
+    if !(error != nil) {
+    println("hereeee")
+    self.friend = user as! PFUser!
+    self.queryPending()
+    }
+    else{
+    println(error)
+    }
+    }
+    
+    }
+    
+    func queryPending(){
+    
+    var pendingQuery = PFQuery(className:"PendingTable")
+    pendingQuery.whereKey("to", equalTo: PFUser.currentUser()!)
+    pendingQuery.whereKey("from", equalTo: self.friend)
+    pendingQuery.findObjectsInBackgroundWithBlock {
+    (requests: [AnyObject]?, error: NSError?) -> Void in
+    
+    if error == nil {
+    for request in requests! {
+    request.deleteInBackground()
+    self.addToEachFriendsTable()
+    NSNotificationCenter.defaultCenter().postNotificationName("load", object: nil)
+    }
+    } else {
+    println("Error: \(error!) \(error!.userInfo!)")
+    }
+    }
+    
+    }
+    
+    */
 }
